1e53ef97df016ed3036960b70fb0c0d5b2dd5a85
Ruud Koot <inbox@ruudkoot.nl>
2023-04-25 22:43:44 +0100

    Helios.Data.List

--- Helios/Data/List.hs
+++ Helios/Data/List.hs
@@ -5,6 +5,9 @@ module Helios.Data.List
 , isSingleton
 , slide
 , unzipL
+, dropAt
+, insertAt
+, updateAt
 ) where
 
 import Data.List
@@ -15,9 +18,31 @@ isSingleton :: [a] -> Bool
 isSingleton [_] = True
 isSingleton _   = False
 
-slide :: (a -> a -> a) -> [a] -> [a]
+slide :: (a -> a -> b) -> [a] -> [b]
 slide f (x1:x2:[]) = [f x1 x2]
 slide f (x1:x2:xs) = f x1 x2 : slide f (x2:xs)
 
 unzipL :: [(a,a)] -> [[a]]
 unzipL xs = let (x,y) = unzip xs in [x,y]
+
+--------------------------------------------------------------------------------
+-- 'at'
+--------------------------------------------------------------------------------
+
+dropAt :: Int -> Int -> [a] -> [a]
+dropAt n m xs
+  = xs1 ++ drop m xs2
+  where
+    (xs1, xs2) = splitAt n xs
+
+insertAt :: Int -> a -> [a] -> [a]
+insertAt n x xs
+  = xs1 ++ x : xs2
+  where
+    (xs1, xs2) = splitAt n xs
+
+updateAt :: Int -> (a -> a) -> [a] -> [a]
+updateAt n f xs
+  = xs1 ++ f x : xs2
+  where
+    (xs1, x:xs2) = splitAt n xs
